362f9faa934a37e2b5cdeacbe408102df7c328a2
Victor Nilsson <victor.nilsson@etraveligroup.com>
2024-08-04 13:57:05 +0200

    wip

--- config/extras/nvim-ansible.nix
+++ config/extras/nvim-ansible.nix
@@ -7,7 +7,7 @@
         owner = "mfussenegger";
         repo = "nvim-ansible";
         rev = "main";
-        sha256 = "1p12189367x0x26cys9wxipzwr3i0bmz4lb0s79ki0a49d6zja2c";
+        sha256 = "";
       };
     })
   ];
