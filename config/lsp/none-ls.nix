49049df0bdc92cb68dd113b74e47e118892696ea
vinylen <victor@viclab.se>
2024-08-19 19:29:58 +0200

    Re-add terraform fmt

--- config/lsp/none-ls.nix
+++ config/lsp/none-ls.nix
@@ -7,7 +7,7 @@
         golangci_lint.enable = true;
         ktlint.enable = true;
         statix.enable = true;
-        # terraform_validate.enable = true;
+        terraform_validate.enable = true;
       };
       formatting = {
         fantomas.enable = true;
@@ -19,7 +19,7 @@
         markdownlint.enable = true;
         shellharden.enable = true;
         shfmt.enable = true;
-        # terraform_fmt.enable = true;
+        terraform_fmt.enable = true;
       };
     };
   };
